341971fef1d7b28cf3ebdad42e1c7ea0b129b2f3
unknown <jefersonsoares1995@gmail.com>
2022-06-28 21:48:13 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um cosole log de teste:
+console.log ("O Paulo fez um commit sem vcoe ver na aula")
+console.log("Enviando para o GitHub")
